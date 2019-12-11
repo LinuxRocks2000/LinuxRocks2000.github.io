be924600e39e851eb137698ba3a26788be4b7ff0
Madison clash <weird-pusheen@example.com>
2019-12-10 19:24:45 -0500

    Update again again

--- header.js
+++ header.js
@@ -1,3 +1,9 @@
 self=document.currentScript;
 header_div_bottom=document.createElement("div");
 self.parentNode.appendChild(header_div_bottom);
+header_div_bottom.id="header";
+header_div_bottom.innerHTML=`
+
+`
+header_div_bottom.style.backgroundColor="lightblue";
+header_div_bottom.style.height="100px";
