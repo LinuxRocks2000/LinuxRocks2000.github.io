72a314681c18f60e05a60eafe4fca13cb86c0049
Madison clash <weird-pusheen@example.com>
2019-12-19 17:42:18 -0500

    f

--- header.js
+++ header.js
@@ -3,6 +3,6 @@ header_div_bottom=document.createElement("div");
 self.parentNode.appendChild(header_div_bottom);
 header_div_bottom.id="header";
 header_div_bottom.innerHTML=`
-
+This site is under repair. This banner will improve (hopefully).
 `;
 header_div_bottom.style.backgroundColor="lightblue";
