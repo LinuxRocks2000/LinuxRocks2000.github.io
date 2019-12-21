2316d8fee85d590f646d41ec41c4ae024b6f1c31
Madison clash <weird-pusheen@example.com>
2019-12-21 10:49:10 -0500

    f

--- header.js
+++ header.js
@@ -2,7 +2,14 @@ self=document.currentScript;
 header_div_bottom=document.createElement("div");
 self.parentNode.appendChild(header_div_bottom);
 header_div_bottom.id="header";
+header_div_bottom.setAttribute("style",`
+padding:20px;
+font-size:20px;
+font-family:sans-serif;
+text-align:center;
+margin-bottom:0px;
+`)
 header_div_bottom.innerHTML=`
-This site is under repair. This banner will improve (hopefully).
+This site is currently under maintenance. Current functionality is limited and some features may fail randomly without reason.
 `;
 header_div_bottom.style.backgroundColor="lightblue";
