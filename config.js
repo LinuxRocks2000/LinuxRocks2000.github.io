49de58d9aa71c304a43c1f1bb4031c8b9e6acba3
LinuxRocks2000 <you@example.com>
2020-01-07 12:49:00 -0500

    f

--- config.js
+++ config.js
@@ -140,6 +140,7 @@
         ["Iceberg part 11","iceberg_11.svg","December 6, 2019","Sorry for the unfinished comic yesterday. I guess I got absent minded?","Isaac finds a nice cave to play his piano in"],
         ["Stu Brunswickson part 1","stu_brunswickson_1.svg","December 8, 2019","Ouch.","Isaac strangely gets locked in the ring with Stu Brunswickson"],
         ["Advent Calendar","advent_calendar.svg","December 9, 2019","The long awaited advent calendar! Until the 25th, I will probably post <i>behind</i> the calendar, so you have to go back one to see the recent comics. New ornaments pop up every day.","The advent calendar"],
-        ["Stu Brunswickson part 2","stu_brunswickson_2.svg","December 22, 2019","Sorry about the missed comics. My site was (and, at the moment, is) under renovation and at the time of writing I just got it working at the most basic functionality.","Isaac finds Seymour in the ring as well"]
+        ["Stu Brunswickson part 2","stu_brunswickson_2.svg","December 22, 2019","Sorry about the missed comics. My site was (and, at the moment, is) under renovation and at the time of writing I just got it working at the most basic functionality.","Isaac finds Seymour in the ring as well"],
+        ["Stu Brunswickson part 3","stu_brunswickson_3.svg","January 7, 2019","I'm sorry, you say your looking for a place called Stu Brunswickson?","Seymour confuses Isaac more"]
     ]
 }
