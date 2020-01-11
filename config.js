ce1bf3301b7f8d68827d8491e4b2f46dc9ebf236
LinuxRocks2000 <you@example.com>
2020-01-10 19:56:13 -0500

    f

--- config.js
+++ config.js
@@ -142,6 +142,7 @@
         ["Advent Calendar","advent_calendar.svg","December 9, 2019","The long awaited advent calendar! Until the 25th, I will probably post <i>behind</i> the calendar, so you have to go back one to see the recent comics. New ornaments pop up every day.","The advent calendar"],
         ["Stu Brunswickson part 2","stu_brunswickson_2.svg","December 22, 2019","Sorry about the missed comics. My site was (and, at the moment, is) under renovation and at the time of writing I just got it working at the most basic functionality.","Isaac finds Seymour in the ring as well"],
         ["Stu Brunswickson part 3","stu_brunswickson_3.svg","January 7, 2019","I'm sorry, you say your looking for a place called Stu Brunswickson?","Seymour confuses Isaac more"],
-        ["Stu Brunswickson part 4","stu_brunswickson_4.svg","January 9, 2019","Bad Seymour. Go sit in a corner. Your such a cute wittle Seymour.","It is clarified how they got in the ring"]
+        ["Stu Brunswickson part 4","stu_brunswickson_4.svg","January 9, 2019","Bad Seymour. Go sit in a corner. Your such a cute wittle Seymour.","It is clarified how they got in the ring"],
+        ["World Domination part 1","world_domination_1.svg","January 10, 2019","For people with small screens, the text on his robe is 'Ponies'","Katherine tries to get Isaac out of bed"]
     ]
 }
