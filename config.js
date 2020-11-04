02317e17926312f640778cb0cf18b7a9b2537398
LinuxRocks2000 <LinuxRocks2000>
2020-11-03 19:59:04 -0500

    210!

--- config.js
+++ config.js
@@ -206,6 +206,7 @@
         ["Religion","religion.svg","October 28, 2020","Finally! I've done it! A comic that can offend anyone, no matter what their religious viewpoint!","Don't worry, this isn't actually offensive at all. (At least not if your intelligent.)"],
         ["Politics","politics.svg","Octover 30, 2020","Another one, Eh?","Just read the description for the last comic."],
         ["The Great Pumpkin","the_great_pumpkin.svg","October 31, 2020","And now you understand the joke - or not. Read the last two comics (including the titles) and you'll probably get it.","Fwip Fwip Fwip Fwip Fwip"],
-        ["Date","date.svg","November 2, 2020","Isaac remains ridiculous.","Don't worry, this comic is 'child friendly'"]
+        ["Date","date.svg","November 2, 2020","Isaac remains ridiculous.","Don't worry, this comic is 'child friendly'"],
+        ["Voting","voting_2020.svg","November 3, 2020","Actually, Isaac's speech was entirely copied from <a href='https://www.usa.gov/election'>https://www.usa.gov/election</a>. Click on the 'Primaries, Caucuses, and Political Conventions' button.","Remember to vote this year, because it really does matter."]
     ]
 }
