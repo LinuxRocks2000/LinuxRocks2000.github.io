13a4bfbfbbd6ec6b6b33fbf3b368060fd465bcca
LinuxRocks2000 <LinuxRocks2000>
2020-12-14 20:02:07 -0500

    228

--- config.js
+++ config.js
@@ -224,6 +224,7 @@
         ["Oh Christmas Tree","oh_christmas_tree.svg","December 6, 2020","Actually, I'm not sure thats possible.","This is an extremely weird comic."],
         ["Marvin","marvin.svg","December 8, 2020","Read the hitchikers guide to the galaxy (book one in the trilogy of five) and you'll get this.","Book one in the trilogy of five - not."],
         ["Trolls 2","trolls_2.svg","December 10, 2020","I can't think of any movie more worthy of mocking than <i>Trolls 2</i>, and I haven't even seen it.","Not for five-year-old girls."],
-        ["Gingerbread House","gingerbread_house.svg","December 12, 2020","Isaacs dad looks exactly like Isaac - Isaac is the one who holds the oven tray.","Smoking is really stupid."]
+        ["Gingerbread House","gingerbread_house.svg","December 12, 2020","Isaacs dad looks exactly like Isaac - Isaac is the one who holds the oven tray.","Smoking is really stupid."],
+        ["Coal","coal.svg","December 14, 2020","",""]
     ]
 }
