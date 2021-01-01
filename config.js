023dad0d25abc906347ef9e27a99241ece1932ea
LinuxRocks2000 <LinuxRocks2000>
2021-01-01 18:07:04 -0500

    Rather unorthodox post this time

--- config.js
+++ config.js
@@ -228,6 +228,7 @@
         ["Coal","coal.svg","December 14, 2020","",""],
         ["C#","c_sharp.svg","December 16, 2020","Twist ending, Eh?","I'm not in fact a Canadian - but I hope to move there someday."],
         ["Christmas Poem","christmas_poem.svg","December 18, 2020","I know its stupid and cheesy beyond belief. I couldn't think of anything else.","Don't read it if you think this is a quality comic."],
-        ["Disgruntled Reindeer","disgruntled_reindeer.svg","December 20, 2020","Posts will likely be spotty for this week. I definitely won't be posting on the 24th/25th/26th.","You don't have to watch Christmas Vacation to get this comic, but you do have to watch Christmas Vacation to be a decent human being."]
+        ["Disgruntled Reindeer","disgruntled_reindeer.svg","December 20, 2020","Posts will likely be spotty for this week. I definitely won't be posting on the 24th/25th/26th.","You don't have to watch Christmas Vacation to get this comic, but you do have to watch Christmas Vacation to be a decent human being."],
+        ["2020","2020.svg","January 1, 2020","Yes, I know its been ten days. I only intend to post comics if I have a good idea. Important note: I definitely want to offend radical far-rightists. Normal republicans can easily be intelligent - I know this for a fact because one of the most intelligent people I know is a republican.","F*** 2020 and lets not have a repeat."]
     ]
 }
